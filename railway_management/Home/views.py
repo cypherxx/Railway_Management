4519dbd7d9cd207fcda71ff806c6b22a29d167e1
cypherxx <65460055+cypherxx@users.noreply.github.com>
2021-04-24 11:14:20 +0530

    front end modified

--- railway_management/Home/views.py
+++ railway_management/Home/views.py
@@ -1,13 +1,10 @@
 from django.shortcuts import render,redirect,HttpResponse,Http404,HttpResponsePermanentRedirect
 
 def index(request):
-
     return render(request,'HTML/index.html')
 
 def get_train_details(request):
-
     return render(request,'HTML/get_train_details.html')
 
 def book_seat(request):
-
     return render(request,'HTML/booking.html')
