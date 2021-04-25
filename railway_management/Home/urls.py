332f33cef8a0c4cc714d30fe68ac505e60e54186
cypherxx <65460055+cypherxx@users.noreply.github.com>
2021-04-25 17:08:13 +0530

    train details added

--- railway_management/Home/urls.py
+++ railway_management/Home/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('train_details',views.train_details,name='gtd'),
     path('book_details',views.book_seat,name='book'),
     path('train_list',views.get_train_list,name='train_list'),
-    path('train_detail',views.get_train_info,name='train_detail')
+    
+    
 ]
--- railway_management/Home/views.py
+++ railway_management/Home/views.py
@@ -21,27 +21,47 @@ def get_train_list(request):
         myvar= cur.var(cx_Oracle.CURSOR)
         cur.callfunc('get_train_names',myvar,[source_name,dest_name])
         train_list1=myvar.getvalue().fetchall()
-        print(train_list1)
-        return render(request,'HTML/train_names.html',{'t_names':train_list1,'s':source_name,'d':dest_name})
+        train_list1=list(sum(train_list1, ()))
+        final={}
+        for i in train_list1:
+            myvar3= cur.var(cx_Oracle.CURSOR)
+            cur.callfunc('get_train_details',myvar3,[i])
+            train_list2=myvar3.getvalue().fetchall()
+            train_list2=list(sum(train_list2,()))
+            myvar2= cur.var(cx_Oracle.CURSOR)
+            s=train_list2[0]
+            cur.callfunc('get_train_route',myvar2,[s])
+            t=myvar2.getvalue().fetchall()
+            train_info_list=list(sum(t, ()))
+            train_list2.append(train_info_list)
+            final[i]=train_list2
+        print(final)
+        
+        return render(request,'HTML/train_names.html',{'t_names':train_list1,'s':source_name,'d':dest_name,'details':final})
+from django.template.defaulttags import register
+...
+@register.filter
+def get_item(dictionary, key):
+    return dictionary.get(key)
 
-def get_train_info(request):
-    if request.method=='POST':
-        train_name=request.POST.get('term')
-        print(train_name,'Vaishnavi')
-        conn=cx_Oracle.connect('qwerty','qwerty','localhost/orcl',encoding = 'UTF-8')
-        cur=conn.cursor()
-        myvar= cur.var(cx_Oracle.CURSOR)
-        cur.callfunc('get_train_details',myvar,[train_name])
-        train_list1=myvar.getvalue().fetchall()
-        train_list1=list(sum(train_list1,()))
-        myvar2= cur.var(cx_Oracle.CURSOR)
-        s=train_list1[0]
-        cur.callfunc('get_train_route',myvar2,[s])
-        t=myvar2.getvalue().fetchall()
-        train_info_list=list(sum(t, ()))
-        train_list1.append(train_info_list)
-        print(train_list1)
-        return render(request,'HTML/train_names.html',{'t_names2':train_list1,'route':train_info_list})
+# def get_train_info(request):
+#     if request.method=='POST':
+#         train_name=request.POST.get('term')
+#         print(train_name)
+#         conn=cx_Oracle.connect('qwerty','qwerty','localhost/orcl',encoding = 'UTF-8')
+#         cur=conn.cursor()
+#         myvar3= cur.var(cx_Oracle.CURSOR)
+#         cur.callfunc('get_train_details',myvar3,[train_name])
+#         train_list2=myvar3.getvalue().fetchall()
+#         train_list2=list(sum(train_list1,()))
+#         myvar2= cur.var(cx_Oracle.CURSOR)
+#         s=train_list2[0]
+#         cur.callfunc('get_train_route',myvar2,[s])
+#         t=myvar2.getvalue().fetchall()
+#         train_info_list=list(sum(t, ()))
+#         train_list2.append(train_info_list)
+#         print(train_list2)
+#         return render(request,'HTML/train_names.html',{'t_names2':train_list1,'route':train_info_list})
 
 
 
